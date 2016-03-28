09c5cd63500f5efec3318bd40b5fe2a41c422c83
tanya1994 <tanya1994@ufl.edu>
2016-03-28 14:29:16 -0400

    checking git

--- app/src/main/java/com/example/sangeetha/hciapp/MainActivity.java
+++ app/src/main/java/com/example/sangeetha/hciapp/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    // comment from tanya
     Button btnSignIn, btnSignUp;
     LoginDataBaseAdapter loginDataBaseAdapter;
 
